a2e00d2524223e07dcde35c23c1a794a8d4ff2cc
nathan854 <43753656+nathan854@users.noreply.github.com>
2018-10-01 17:44:33 +0300

    Update nath.js

--- nath.js
+++ nath.js
@@ -1,17 +1,42 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+console.log("Welcome");
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag} !`);
+ 
+});
+ 
+client.on('guildMemberAdd',async member => {
+  if(member.guild.id !== '462347265747451914') return;
+  setTimeout(function(){
+  member.guild.channels.find(r => r.id === '492406462476451840').send('**Welcome to Bubbles Tumblr**');
+},3000);
+});
+var PrEfix = "$$";
 client.on('message', message => {
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`nathan ${x}`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
+  if (!message.content.startsWith(PrEfix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '442756928624656405') return;
+if (message.content.startsWith(PrEfix + 'wt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(PrEfix + 'ls')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(PrEfix + 'pl')) {
+client.user.setActivity(argresult, {type:'PLAYING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(PrEfix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
 });
 client.login(process.env.BOT_TOKEN);
