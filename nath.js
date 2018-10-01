f1a2f713a5c1b6dda2437be40b70d1d5dfec3707
nathan854 <43753656+nathan854@users.noreply.github.com>
2018-10-01 20:05:57 +0300

    Update nath.js

--- nath.js
+++ nath.js
@@ -3,17 +3,6 @@ const client = new Discord.Client();
 
 console.log("Welcome");
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag} !`);
- 
-});
- 
-client.on('guildMemberAdd',async member => {
-  if(member.guild.id !== '462347265747451914') return;
-  setTimeout(function(){
-  member.guild.channels.find(r => r.id === '492406462476451840').send('**Welcome to Bubbles Tumblr**');
-},3000);
-});
 var PrEfix = "$$";
 client.on('message', message => {
   if (!message.content.startsWith(PrEfix)) return;
